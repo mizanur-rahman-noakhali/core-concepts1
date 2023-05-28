0425b6b36086006a3dcb8e1da8f65735161e8011
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-05-28 13:27:42 +0600

    props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 
 function App() {
  const nayks=['alomgir','hero Alom','Akash','salman']
+ const Products=[
+  {name:'photoshop',price:'$90.99'},
+  {name:'ilistrator',price:'$60'},
+  {name:'pdf reader',price:'#20'}
+]
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +17,9 @@ function App() {
         <Person nayok='salman' nayka='katrina'></Person>
         <Person nayak='josim' nayka='sabana'></Person>
         <Person nayak='sharuk' nayka='kazol'></Person> */}
+        <Product product={Products[0]}></Product>
+        <Product product={Products[1]}></Product>
+        <Product product={Products[2]}></Product>
         <Person name='Munna' job='Football'></Person>
         <Person name='masum' job='Dorsok'></Person>
       </header>
@@ -31,11 +39,32 @@ function App() {
 //    </div>
 //    )
 // }
+
+function Product(props){
+  const ProductStyle={
+    border:'1px solid red',
+    borderRadius:'5px',
+    backgroundColor:'gray',
+    hight:'200px',
+    width:'200px',
+    float:'left'
+  }
+  const {name,price}=props.product;
+  console.log(name,price)
+  return(
+    <div style={ProductStyle}>
+   <h3>{name}</h3>
+   <h5> {price}</h5>
+   <button>Buy Now</button>
+    </div>
+  )
+}
+
 function Person(props){
 
 return(
   <div style={{border:'2px solid gold' ,width:'200px',padding:'5px'}}>
-    <h3>My Name:{props.name}</h3>
+    <h3>Name:{props.name}</h3>
     <p>My Profession:{props.job}</p>
   </div>
 )
